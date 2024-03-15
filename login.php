1e2704fdde09fcb22b815c70772a2d5a20bd3310
LiudmylaWeb <141134148+LiudmylaWeb@users.noreply.github.com>
2024-03-15 09:39:32 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,38 +1,22 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "banque";
-
-// Créer la connexion
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-    die("Échec de la connexion : " . $conn->connect_error);
-}
-
-$login = $conn->real_escape_string($_POST['login']);
-$motDePasse = $conn->real_escape_string($_POST['motDePasse']);
-
-$sql = "SELECT categorie FROM Employe WHERE login='$login' AND motDePasse='$motDePasse'";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // Utilisateur authentifié
-    $row = $result->fetch_assoc();
-    $categorie = $row["categorie"];
-    
-    // Redirection basée sur la catégorie
-    if ($categorie == "directeur") {
-        header("Location: directeur.php");
-    } elseif ($categorie == "conseiller") {
-        header("Location: conseiller.php");
-    } elseif ($categorie == "agent") {
-        header("Location: agent.php");
-    }
-} else {
-    echo "Identifiant ou mot de passe incorrect.";
-}
-$conn->close();
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Connexion</title>
+</head>
+<body>
+    <h2>Connexion</h2>
+    <form action="authentification.php" method="post">
+        <p>
+            <label for="">Identifiant :</label>
+            <input type="text" name="login" required>
+        </p>
+        <p>
+            <label for="">Mot de passe :</label>
+            <input type="password" name="motDePasse" required>
+        </p>
+        <p>
+            <input type="submit" value="Connexion">
+        </p>
+    </form>
+</body>
+</html>
