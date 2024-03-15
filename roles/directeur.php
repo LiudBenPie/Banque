ab1c0ff59d4aa2fadb71893a1d3d5f5a55c5cb91
LiudmylaWeb <141134148+LiudmylaWeb@users.noreply.github.com>
2024-03-15 09:48:40 +0200

    Update directeur.php

--- roles/directeur.php
+++ roles/directeur.php
@@ -1,1 +1,8 @@
-
+<h1>Accueil Directeur</h1>
+        <p>Bienvenue dans votre espace de gestion. Veuillez choisir l'une des options ci-dessous pour commencer.</p>
+        <ul>
+            <li><a href="gestion-utilisateurs.html">Gestion des Utilisateurs</a></li>
+            <li><a href="gestion-contrats.html">Gestion des Contrats</a></li>
+            <li><a href="gestion-documents.html">Gestion des Documents</a></li>
+            <li><a href="statistiques.html">Statistiques</a></li>
+        </ul>
