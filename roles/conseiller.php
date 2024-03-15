b53b2e58f03ddd743d56350a9d8b2f3a5cb439e7
LiudmylaWeb <141134148+LiudmylaWeb@users.noreply.github.com>
2024-03-15 10:27:28 +0200

    Update conseiller.php

--- roles/conseiller.php
+++ roles/conseiller.php
@@ -1,10 +1,10 @@
 <h1>Espace Conseiller</h1>
     <nav>
         <ul class="nav-links">
-            <li><a href="visualiser-planning.html" class="link">Visualiser le planning de la journée</a></li>
-            <li><a href="planning-date.html" class="link">Visualiser le planning sur une date spécifique</a></li>
-            <li><a href="bloquer-creneaux.html" class="link">Bloquer des créneaux horaires</a></li>
-            <li><a href="synthese-client.html" class="link">Visualiser la synthèse du client</a></li>
+            <!-- <li><a href="visualiser-planning.html" class="link">Visualiser le planning de la journée</a></li>
+            <li><a href="planning-date.html" class="link">Visualiser le planning sur une date spécifique</a></li> -->
+            <!-- <li><a href="bloquer-creneaux.html" class="link">Bloquer des créneaux horaires</a></li> -->
+            <!-- <li><a href="synthese-client.html" class="link">Visualiser la synthèse du client</a></li> -->
             <li><a href="inscrire-client.html" class="link">Inscrire un nouveau client</a></li>
             <li><a href="vendre-contrat.html" class="link">Vendre un contrat à un client</a></li>
             <li><a href="ouvrir-comptes.html" class="link">Ouvrir un ou plusieurs comptes pour un client</a></li>
