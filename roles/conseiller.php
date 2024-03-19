8851ca5b6517ddbbe0a21cf79bc52f7a22b701a3
LiudmylaWeb <141134148+LiudmylaWeb@users.noreply.github.com>
2024-03-19 21:52:07 +0200

    Update conseiller.php

--- roles/conseiller.php
+++ roles/conseiller.php
@@ -6,7 +6,7 @@
             <!-- <li><a href="bloquer-creneaux.html" class="link">Bloquer des créneaux horaires</a></li> -->
             <!-- <li><a href="synthese-client.html" class="link">Visualiser la synthèse du client</a></li> -->
             <li><a href="inscrire-client.html" class="link">Inscrire un nouveau client</a></li>
-            <li><a href="vendre-contrat.html" class="link">Vendre un contrat à un client</a></li>
+            <li><a href="../vendre-contrat.html" class="link">Vendre un contrat à un client</a></li>
             <li><a href="ouvrir-comptes.html" class="link">Ouvrir un ou plusieurs comptes pour un client</a></li>
             <li><a href="modifier-decouvert.html" class="link">Modifier la valeur d'un découvert</a></li>
             <li><a href="resilier-contrat-compte.html" class="link">Résilier un contrat ou un compte</a></li>
