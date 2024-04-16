78ed75e842e62c774424c313d27029341abe24b8
LiudmylaWeb <liudmylalahoida@gmail.com>
2024-04-16 22:39:25 +0300

    rechercherDecouvert

--- views/roles/conseiller.php
+++ views/roles/conseiller.php
@@ -7,7 +7,7 @@
             <!-- <li><a href="synthese-client.html" class="link">Visualiser la synthèse du client</a></li> -->
             <li><a href="../ajout_client_conseiller.php" class="link">Inscrire un nouveau client</a></li>
             <li><a href="../vendre-un-contrat-conseiller.php" class="link">Vendre un contrat à un client</a></li>
-            <li><a href="ouvrir-comptes.html" class="link">Ouvrir un ou plusieurs comptes pour un client</a></li>
+            <li><a href="../rechercherDecouvert.php" class="link">Ouvrir un ou plusieurs comptes pour un client</a></li>
             <li><a href="modifier-decouvert.html" class="link">Modifier la valeur d'un découvert</a></li>
             <li><a href="resilier-contrat-compte.html" class="link">Résilier un contrat ou un compte</a></li>
         </ul>
