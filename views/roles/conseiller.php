c182a789469843f2277facb9b35844fb6fca6259
benoit55565 <tonnoir.benoit.bt@gmail.com>
2024-04-19 23:37:15 +0200

    benoit

--- views/roles/conseiller.php
+++ views/roles/conseiller.php
@@ -5,8 +5,8 @@
             <li><a href="planning-date.html" class="link">Visualiser le planning sur une date spécifique</a></li> -->
             <!-- <li><a href="bloquer-creneaux.html" class="link">Bloquer des créneaux horaires</a></li> -->
             <!-- <li><a href="synthese-client.html" class="link">Visualiser la synthèse du client</a></li> -->
-            <li><a href="../ajout_client_conseiller.php" class="link">Inscrire un nouveau client</a></li>
-            <li><a href="../vendre-un-contrat-conseiller.php" class="link">Vendre un contrat à un client</a></li>
+            <li><a href="../ajouterClient.php" class="link">Inscrire un nouveau client</a></li>
+            <li><a href="../vendreContrat.php" class="link">Vendre un contrat à un client</a></li>
             <li><a href="../ouvrirCompte.php" class="link">Ouvrir un ou plusieurs comptes pour un client</a></li>
             <li><a href="../rechercherDecouvert.php" class="link">Modifier la valeur d'un découvert</a></li>
             <li><a href="resilier-contrat-compte000.html" class="link">Résilier un contrat ou un compte</a></li>
