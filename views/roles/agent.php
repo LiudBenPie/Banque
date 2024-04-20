5812b0a01e9ebbf3e65726023d8fc2afa0a81d80
benoit55565 <tonnoir.benoit.bt@gmail.com>
2024-04-20 18:12:47 +0200

    change

--- views/roles/agent.php
+++ views/roles/agent.php
@@ -1,11 +1,7 @@
 <h1>Espace Agent</h1>
     <nav><ul>
-<<<<<<< HEAD
-        <li><a href="../liudtest.html">Prendre un rendez-vous client</a></li>
-=======
         <li><a href="/pages/rdv.php">RDV</a></li>
         <li><a href="site.php?menu=rdv_client.html">Prendre un rendez-vous client</a></li>
->>>>>>> 093977fe5ccc37467d0f55233cbe039c2fab1a47
         <li><a href="site.php?menu=depot_retrait.html">Réaliser un dépôt ou un retrait</a></li>
         <li><a href="site.php?menu=synthese_client">Synthèse client</a></li>
         <li><a href="../rechercherClient.php">Modifier les informations client</a></li>
