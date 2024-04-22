a563e767582a20fc3e292f9b64ffcabbea478d7f
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-22 12:48:24 +0000

    css upgrade

--- rechercherTypeContrat.php
+++ rechercherTypeContrat.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rechercher Type Contrat</title>
-    <link rel="stylesheet" href="styles.css"> <!-- Lien vers votre fichier CSS -->
+    <link rel="stylesheet" href="styles.css">
 </head>
 
 <body>
