4f7fff3d902d393bdf30976701d99f865595bc99
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-23 21:15:40 +0000

    correction SQL

--- rechercherCompte.php
+++ rechercherCompte.php
@@ -3,9 +3,9 @@ require('init.php');
 checkAcl('auth');
 include VIEWS_DIR . '/menu.php';
 // Récupération de la liste des comptes clients avec les informations sur le client
-$sql = "SELECT cc.idCompteClient, cc.numClient, c.nom, c.prenom, cc.idCompte 
-        FROM CompteClient cc 
-        INNER JOIN Client c ON cc.numClient = c.numClient";
+$sql = "SELECT CompteClient.idCompteClient, CompteClient.numClient, Client.nom, Client.prenom, CompteClient.idCompteClient 
+        FROM CompteClient 
+        INNER JOIN Client ON CompteClient.numClient = Client.numClient";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $compteClients = $stmt->fetchAll();
@@ -15,7 +15,7 @@ $compteClients = $stmt->fetchAll();
     <select name="idCompteClient" id="compteClient">
         <?php foreach ($compteClients as $compteClient) : ?>
             <option value="<?php echo $compteClient['idCompteClient']; ?>">
-                <?php echo "Client: " . $compteClient['nom'] . ' ' . $compteClient['prenom'] . ", ID Compte: " . $compteClient['idCompte']; ?>
+                <?php echo "Client: " . $compteClient['nom'] . ' ' . $compteClient['prenom'] . ", N° Compte: " . $compteClient['idCompte']; ?>
             </option>
         <?php endforeach; ?>
     </select>
