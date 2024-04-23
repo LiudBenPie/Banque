321de673a45af6cf5a8bc638787f9a4f657e7ae2
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-23 21:05:57 +0000

    correctif

--- rechercherCompte.php
+++ rechercherCompte.php
@@ -2,20 +2,22 @@
 require('init.php');
 checkAcl('auth');
 include VIEWS_DIR . '/menu.php';
-// Récupération de la liste des comptes clients
-$sql = "SELECT idCompteClient, numClient FROM CompteClient";
+// Récupération de la liste des comptes clients avec les informations sur le client
+$sql = "SELECT cc.idCompteClient, cc.numClient, c.nom, c.prenom, cc.dateOuverture, cc.solde, cc.montantDecouvert, cc.idCompte 
+        FROM CompteClient cc 
+        INNER JOIN Client c ON cc.numClient = c.numClient";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-$comptesClients = $stmt->fetchAll();
+$compteClients = $stmt->fetchAll();
 ?>
 <form action="realiserOperation.php" method="post">
-    <label for="compteClient">Sélectionnez le compte client pour l'opération :</label>
+    <label for="compteClient">Sélectionnez le compte client à modifier :</label>
     <select name="idCompteClient" id="compteClient">
-        <?php foreach ($comptesClients as $compteClient) : ?>
+        <?php foreach ($compteClients as $compteClient) : ?>
             <option value="<?php echo $compteClient['idCompteClient']; ?>">
-                <?php echo $compteClient['numClient']; ?>
+                <?php echo "Client: " . $compteClient['nom'] . ' ' . $compteClient['prenom'] . ", Date d'ouverture: " . $compteClient['dateOuverture'] . ", Solde: " . $compteClient['solde'] . ", Montant découvert: " . $compteClient['montantDecouvert'] . ", ID Compte: " . $compteClient['idCompte']; ?>
             </option>
         <?php endforeach; ?>
     </select>
-    <button type="submit">Réaliser une opération</button>
+    <button type="submit">Modifier</button>
 </form>
