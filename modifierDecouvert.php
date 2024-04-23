25464d70ad9e052c425a5e866f9439aad78ea91c
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-23 18:12:24 +0000

    j'ai pas d'idée

--- modifierDecouvert.php
+++ modifierDecouvert.php
@@ -6,18 +6,19 @@ include VIEWS_DIR . '/menu.php';
 $updateSuccessful = false;
 
 if (isset($_POST['numClient'])) {
-    $numClient = intval($_POST['numClient']); // Assurez-vous que $numClient est un entier
+    $numClient = $_POST['numClient'];
 
     if (isset($_POST['action']) && $_POST['action'] === 'modifier') {
         // Traitement de la modification du découvert du compte sélectionné
-        $idCompte = intval($_POST['idCompte']); // Assurez-vous que $idCompte est un entier
-        $nouveauDecouvert = floatval($_POST['nouveauDecouvert']); // Assurez-vous que $nouveauDecouvert est un flottant
+        $idCompte = $_POST['idCompte'];
+        $nouveauDecouvert = $_POST['nouveauDecouvert'];
         $sql = "UPDATE CompteClient SET montantDecouvert = ? WHERE numClient = ? AND idCompte = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$nouveauDecouvert, $numClient, $idCompte]);
 
         $_SESSION['updateSuccess'] = true;
         $updateSuccessful = true; 
+
     } else {
         // Requête pour récupérer les identifiants et les découverts des comptes associés à ce client
         $sqlComptes = "SELECT idCompte, montantDecouvert FROM CompteClient WHERE numClient = ?";
@@ -35,14 +36,7 @@ if ($updateSuccessful) {
 
 <form action="modifierDecouvert.php" method="post" name='monForm'>
     <input type="hidden" name="numClient" value="<?php echo isset($_POST['numClient']) ? htmlspecialchars($_POST['numClient']) : ''; ?>">
-    <p>
-        <label for="idCompte">Sélectionnez un compte :</label>
-        <select name="idCompte">
-            <?php foreach ($comptes as $compte): ?>
-                <option value="<?php echo $compte['idCompte']; ?>"><?php echo $compte['idCompte']; ?></option>
-            <?php endforeach; ?>
-        </select>
-    </p>
+    <input type="hidden" name="idCompte" value="<?php echo isset($_POST['idCompte']) ? htmlspecialchars($_POST['idCompte']) : ''; ?>">
 
     <p>
         <label for="nouveauDecouvert">Nouveau montant autorisé de découvert :</label>
