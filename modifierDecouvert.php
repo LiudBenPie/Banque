b03386607c3c8ea810b68b42d0661a2929c07e6b
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-23 18:08:37 +0000

    correction Decouvert

--- modifierDecouvert.php
+++ modifierDecouvert.php
@@ -6,19 +6,18 @@ include VIEWS_DIR . '/menu.php';
 $updateSuccessful = false;
 
 if (isset($_POST['numClient'])) {
-    $numClient = $_POST['numClient'];
+    $numClient = intval($_POST['numClient']); // Assurez-vous que $numClient est un entier
 
     if (isset($_POST['action']) && $_POST['action'] === 'modifier') {
         // Traitement de la modification du découvert du compte sélectionné
-        $idCompte = $_POST['idCompte'];
-        $nouveauDecouvert = $_POST['nouveauDecouvert'];
+        $idCompte = intval($_POST['idCompte']); // Assurez-vous que $idCompte est un entier
+        $nouveauDecouvert = floatval($_POST['nouveauDecouvert']); // Assurez-vous que $nouveauDecouvert est un flottant
         $sql = "UPDATE CompteClient SET montantDecouvert = ? WHERE numClient = ? AND idCompte = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$nouveauDecouvert, $numClient, $idCompte]);
 
         $_SESSION['updateSuccess'] = true;
         $updateSuccessful = true; 
-
     } else {
         // Requête pour récupérer les identifiants et les découverts des comptes associés à ce client
         $sqlComptes = "SELECT idCompte, montantDecouvert FROM CompteClient WHERE numClient = ?";
@@ -36,7 +35,14 @@ if ($updateSuccessful) {
 
 <form action="modifierDecouvert.php" method="post" name='monForm'>
     <input type="hidden" name="numClient" value="<?php echo isset($_POST['numClient']) ? htmlspecialchars($_POST['numClient']) : ''; ?>">
-    <input type="hidden" name="idCompte" value="<?php echo isset($_POST['idCompte']) ? htmlspecialchars($_POST['idCompte']) : ''; ?>">
+    <p>
+        <label for="idCompte">Sélectionnez un compte :</label>
+        <select name="idCompte">
+            <?php foreach ($comptes as $compte): ?>
+                <option value="<?php echo $compte['idCompte']; ?>"><?php echo $compte['idCompte']; ?></option>
+            <?php endforeach; ?>
+        </select>
+    </p>
 
     <p>
         <label for="nouveauDecouvert">Nouveau montant autorisé de découvert :</label>
