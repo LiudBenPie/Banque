8f5c94ab7f9f5c8aa6164d405ac926e3cf8b12c1
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-04-23 20:56:15 +0000

    correction

--- rechercherCompte.php
+++ rechercherCompte.php
@@ -2,18 +2,18 @@
 require('init.php');
 checkAcl('auth');
 include VIEWS_DIR . '/menu.php';
-// Récupération de la liste des comptes
-$sql = "SELECT idCompte, nomTypeCompte, description FROM compte";
+// Récupération de la liste des comptes clients
+$sql = "SELECT idCompteClient, numClient FROM CompteClient";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
-$comptes = $stmt->fetchAll();
+$comptesClients = $stmt->fetchAll();
 ?>
-<form action="realiserOperation.php" method="post">
-    <label for="compte">Sélectionnez le compte pour l'opération :</label>
-    <select name="idCompte" id="compte">
-        <?php foreach ($comptes as $compte) : ?>
-            <option value="<?php echo $compte['idCompte']; ?>">
-                <?php echo $compte['nomTypeCompte'] . ' - ' . $compte['description']; ?>
+<form action="réaliserOpération.php" method="post">
+    <label for="compteClient">Sélectionnez le compte client pour l'opération :</label>
+    <select name="idCompteClient" id="compteClient">
+        <?php foreach ($comptesClients as $compteClient) : ?>
+            <option value="<?php echo $compteClient['idCompteClient']; ?>">
+                <?php echo $compteClient['numClient']; ?>
             </option>
         <?php endforeach; ?>
     </select>
