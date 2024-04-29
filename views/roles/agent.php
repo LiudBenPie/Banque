321ab2cdbb7033cf47e06b404e1fd7452f1185e6
LiudmylaWeb <liudmylalahoida@gmail.com>
2024-04-29 22:43:39 +0300

    liud

--- views/roles/agent.php
+++ views/roles/agent.php
@@ -1,7 +1,7 @@
 
-<nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #e3f2fd;">
+<nav class="navbar navbar-expand-lg bg-body-tertiary"  style="background-color: #e3f2fd;">
 <div class="container-fluid">
-<a class="navbar-brand" href="/"><img src="/favicon.ico" width="50" alt="icon"></a>
+<a class="navbar-brand" href="/"><img src="/favicon.ico" width="60" alt="icon"></a>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
 </button>
