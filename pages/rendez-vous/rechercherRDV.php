94764421b4b28abaa3d0fc0a1a10fc3bd2fde984
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-05-03 09:09:19 +0000

    rechercher RDV

---
+++ pages/rendez-vous/rechercherRDV.php
@@ -0,0 +1,23 @@
+<?php
+require('../../init.php');
+checkAcl('auth');
+include VIEWS_DIR . '/menu.php';
+// Récupération de la liste des pieces
+$sql = "SELECT numRdv, dateRdv, heureRdv, client.nom, client.prenom,
+        FROM rdv
+        INNER JOIN client on client.numClient = rdv.numclient";
+$stmt = $conn->prepare($sql);
+$stmt->execute();
+$Rdv = $stmt->fetchAll();
+?>
+<form action="modifierRDV.php" method="post">
+    <label for="rdv">Sélectionnez le rendez-vous à modifier :</label>
+    <select name="numRdv" id="numRdv">
+        <?php foreach ($Rdv as $RDV) : ?>
+            <option value="<?php echo $RDV['numRdv']; ?>">
+                <?php echo "RDV du".$RDV['dateRdv']." à ".$RDV['heureRdv']." avec ".$RDV['client.nom']." ".$RDV['client.prenom'] ?>
+            </option>
+        <?php endforeach; ?>
+    </select>
+    <button type="submit">Modifier</button>
+</form>
