457c66e5c353309d1fc3cd152c4c3badd3be8a92
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-05-03 09:26:36 +0000

    rechercher RDV test

--- pages/rendez-vous/rechercherRDV.php
+++ pages/rendez-vous/rechercherRDV.php
@@ -2,20 +2,22 @@
 require('../../init.php');
 checkAcl('auth');
 include VIEWS_DIR . '/menu.php';
-// Récupération de la liste des pieces
-$sql = "SELECT numRdv, dateRdv, heureRdv, client.nom, client.prenom,
+
+// Récupération de la liste des rendez-vous avec les détails clients
+$sql = "SELECT rdv.numRdv, rdv.dateRdv, rdv.heureRdv, client.nom AS client_nom, client.prenom AS client_prenom
         FROM rdv
-        INNER JOIN client on rdv.numclient = client.numClient";
+        INNER JOIN client ON rdv.numclient = client.numClient";
 $stmt = $conn->prepare($sql);
 $stmt->execute();
 $Rdv = $stmt->fetchAll();
 ?>
+
 <form action="modifierRDV.php" method="post">
     <label for="rdv">Sélectionnez le rendez-vous à modifier :</label>
     <select name="numRdv" id="numRdv">
         <?php foreach ($Rdv as $RDV) : ?>
             <option value="<?php echo $RDV['numRdv']; ?>">
-                <?php echo "RDV du".$RDV['dateRdv']." à ".$RDV['heureRdv']." avec ".$RDV['client.nom']." ".$RDV['client.prenom'] ?>
+                <?php echo "RDV du " . $RDV['dateRdv'] . " à " . $RDV['heureRdv'] . " avec " . $RDV['client_nom'] . " " . $RDV['client_prenom']; ?>
             </option>
         <?php endforeach; ?>
     </select>
