2a06a67890525f318ed6db0efe4afd238bf90608
Ibeback28 <159572874+Ibeback28@users.noreply.github.com>
2024-05-03 13:55:12 +0000

    modifier rdv base

--- pages/rendez-vous/modifierRDV.php
+++ pages/rendez-vous/modifierRDV.php
@@ -42,47 +42,61 @@
         }
 
         // Sélectionner à nouveau le motif après la mise à jour ou la suppression
-        $sql = "SELECT * FROM motif WHERE idMotif = ?";
+        $sql = "SELECT * FROM rdv WHERE numRdv = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$idMotif]);
-        $motif = $stmt->fetch();
+        $Rdv = $stmt->fetch();
     }
 
     // Affiche une alerte si la mise à jour a été réussie
     if ($updateSuccessful) {
-        echo '<script>alert("Les informations du motif ont été mises à jour avec succès.");</script>';
+        echo '<script>alert("Le rendez-vous a été mises à jour avec succès.");</script>';
     }
 
     // Affiche une alerte si la suppression a été réussie
     if ($deleteSuccessful) {
-        echo '<script>alert("Le motif a été supprimé avec succès.");</script>';
+        echo '<script>alert("Le rendez-vous a été supprimé avec succès.");</script>';
     }
     ?>
     <!-- Formulaire pour la mise à jour et la suppression des informations du motif -->
-    <form action="modifierPiece.php" method="post" name='monForm'>
+    <form action="modifierRDV.php" method="post" name='monForm'>
 
         <!-- Champs du formulaire avec les informations à jour du motif -->
         <p>
             <input type="hidden" name="numRdv"
-                value="<?php echo isset($motif['numRdv']) ? htmlspecialchars($motif['numRdv']) : ''; ?>">
+                value="<?php echo isset($Rdv['numRdv']) ? htmlspecialchars($Rdv['numRdv']) : ''; ?>">
         </p>
 
         <p>
-            <label for="libelleMotif">Libellé du Motif:</label>
-            <input type="text" id="libelleMotif" name="libelleMotif"
-                value="<?php echo isset($motif['libelleMotif']) ? htmlspecialchars($motif['libelleMotif']) : ''; ?>">
+            <label for="dateRdv">Date du rendez-vous :</label>
+            <input type="date" id="dateRdv" name="dateRdv"
+                value="<?php echo isset($Rdv['dateRdv']) ? htmlspecialchars($Rdv['dateRdv']) : ''; ?>">
         </p>
         <p>
-            <label for="listePieces">Liste des Pièces:</label>
-            <input type="text" id="listePieces" name="listePieces"
-                value="<?php echo isset($motif['listePieces']) ? htmlspecialchars($motif['listePieces']) : ''; ?>">
+            <label for="heureRdv">Heure du rendez-vous :</label>
+            <input type="number" id="heureRdv" name="heureRdv"
+                value="<?php echo isset($Rdv['heureRdv']) ? htmlspecialchars($Rdv['heureRdv']) : ''; ?>">
         </p>
-
+        <p>
+            <label for="numEmploye">Heure du rendez-vous :</label>
+            <input type="number" id="numEmploye" name="numEmploye"
+                value="<?php echo isset($Rdv['numEmploye']) ? htmlspecialchars($Rdv['numEmploye']) : ''; ?>">
+        </p>
+        <p>
+            <label for="idMotif">Heure du rendez-vous :</label>
+            <input type="number" id="idMotif" name="idMotif"
+                value="<?php echo isset($Rdv['idMotif']) ? htmlspecialchars($Rdv['idMotif']) : ''; ?>">
+        </p>
+        <p>
+            <label for="numClient">Heure du rendez-vous :</label>
+            <input type="number" id="numClient" name="numClient"
+                value="<?php echo isset($Rdv['numClient']) ? htmlspecialchars($Rdv['numClient']) : ''; ?>">
+        </p>
         <p>
             <a href="../..">Page précédente</a>
             <button type="submit" name="action" value="modifier">Mettre à jour</button>
             <button type="submit" name="action" value="supprimer"
-                onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce motif ?')">Supprimer</button>
+                onclick="return confirm('Êtes-vous sûr de vouloir supprimer ce rendez-vous ?')">Supprimer</button>
         </p>
     </form>
 </body>
